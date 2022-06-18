b429b7042cc8688b040433fe3a8ec54289fc76d7
Nael <darwichenael@gmail.com>
2022-06-18 16:06:10 +0200

    Added bootstrap and advent of code(day)

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import random
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     lottery_numbers = random.sample(range(1, 50), 6)
     name = 'Nael'
     return render_template('hello.html', name=name, lottery_numbers=lottery_numbers)
 
+@app.route("/advent_of_code")
+def advent_of_code():
+    return 'advent_of_code'
 
+@app.route("/advent_of_code/<int:day>")
+def advent_of_code_day(day):
+    return f"Today is {day}."
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=3000)
